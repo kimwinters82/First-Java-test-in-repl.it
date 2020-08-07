87c1ec115097b545c1298028db059c522198d4f4
kim winters <kim.winters82@gmail.com>
2020-08-07 14:28:10 +0000

    Added the maths for calculating how many plastic products could be saved from landfill

--- Main.java
+++ Main.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
+
+
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-    System.out.println("testing new IDE");
+    int avgMenopauseAge = 51;
+    int avgMenstrualProductPerCycle = 20;
+    int avgMenstrualProductsPerYear = avgMenstrualProductPerCycle * 12;
+    
+    
+    Scanner keyboardInput = new Scanner(System.in);
+    System.out.print("Enter your name: "); 
+    String name = keyboardInput.nextLine();
+
+    System.out.print("Enter your age: "); 
+    int age = keyboardInput.nextInt();
+
+    int yearsLeftToMenstrate = avgMenopauseAge - age;
+    int productsSaved = yearsLeftToMenstrate * avgMenstrualProductsPerYear;
+    System.out.println("");
+    System.out.println("Hey " + name + "!");
+    System.out.println("Did you know that by swithcing to a reusable menstrual product, you would save " + productsSaved + " plastic products from ending up in the environment!");
   }
 }
